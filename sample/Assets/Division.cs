b45e1e8a3d0eae0cca63bb8ea884f1bce4232d4b
Jesus-1996 <38133794+koussaykhelil@users.noreply.github.com>
2018-06-25 12:16:38 +0100

    Sampling

---
+++ sample/Assets/Division.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Division : MonoBehaviour {
+
+    // Use this for initialization
+    public int a;
+    public int b;
+    void division()
+    {
+        Debug.Log(a / b);
+    }
+	void Start () {
+        division();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
